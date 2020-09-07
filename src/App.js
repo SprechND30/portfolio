42790b1bd9c433258386ccae4c949b7866b378d1
Nathaniel Sprecher <sprechernd1@gcc.edu>
2020-09-06 20:54:48 -0400

    removed contact page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import Nav from 'react-bootstrap/Nav';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
 
 class App extends React.Component {
 
@@ -29,9 +28,6 @@ class App extends React.Component {
       },
       about: {
         title: 'About Me'
-      },
-      contact: {
-        title: 'Contact Me!'
       }
     }
   }
@@ -49,14 +45,12 @@ class App extends React.Component {
               <Nav className="ml-auto">
                 <Link className="nav-link" to="/">Home</Link>
                 <Link className="nav-link" to="/about">About</Link>
-                <Link className="nav-link" to="/contact">Contact</Link>
               </Nav>
             </NavBar.Collapse>
           </NavBar>
 
           <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
           <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
-          <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
 
           <Footer />
 
