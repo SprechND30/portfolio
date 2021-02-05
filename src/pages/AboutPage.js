bb0062c9f2e2cafac8bc236d0fbcab97f986be09
Nathaniel Sprecher <sprechernd1@gcc.edu>
2021-02-04 20:38:05 -0500

    Updated font family to Inter

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -11,7 +11,7 @@ function AboutPage(props) {
 
       <Content>
         <p>
-        Hi! I am a recent graduate with a degree in Computer Science from Grove City College, with minors in Mathematics and Data Science. I work well both collaboratively and independently, on projects that span from open-ended to well-defined. I always love tackling hard problems, but more than that, I love learning new ways of approaching them.  
+          Hi! I am a recent graduate with a degree in Computer Science from Grove City College, with minors in Mathematics and Data Science. I work well both collaboratively and independently, on projects that span from open-ended to well-defined. I always love tackling hard problems, but more than that, I love learning new ways of approaching them.  
         </p>
         <p>
           Last year, my senior project gave me the chance to participate in a software development lifecycle with well-defined start and end-points, all while working alongside other highly-motivated individuals. However, I am also comfortable with ambiguous tasks. During my undergraduate studies, I was part of a cross-disciplinary team on a computer vision independent research project. The experience allowed me to approach open-ended problems creatively from diverse perspectives, both collaboratively and independently. Finally, in the summer of 2019, I gained valuable experience working independently as an undergraduate researcher in Washington State's Smart Environments REU. The ambiguous nature of the problems required me to approach them creatively and with self-motivation. 
